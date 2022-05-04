9dffa6f43d1482a00c16c5134fbc57b5eef8b5f3
Shalsh <shrivastava.shal@gmail.com>
2022-05-04 11:35:37 -0500

    Clean up

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ setup(
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
-    # install_requires=["feedparser", "html2text"],
+    # install_requires=[],
     # entry_points={
     #     "console_scripts": [
-    #         "realpython=reader.__main__:main",
+    #
     #     ]
     # },
 )
